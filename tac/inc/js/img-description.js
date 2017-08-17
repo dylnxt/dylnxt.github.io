d3b7d995a2700c08953d8d233497b71706eadadf
dylnxt <dylnxt@gmail.com>
2017-08-17 21:46:02 +1000

    Add files via upload

---
+++ tac/inc/js/img-description.js
@@ -0,0 +1,25 @@
+$(window).load(function(){
+	//for each description div...
+	$('.description').each(function(){
+		//...set the opacity to 0...
+		$(this).css('opacity', 0);
+		//..set width same as the image...
+		$(this).css('width', $(this).siblings('img').width());
+		//...get the parent (the wrapper) and set it's width same as the image width... '
+		$(this).parent().css('width', $(this).siblings('img').width());
+		//...set the display to block
+		$(this).css('display', 'block');
+	});
+	
+	$('.galleryRow li a').hover(function(){
+		//when mouse hover over the wrapper div
+		//get it's children elements with class descriptio
+		//and show it using fadeTo
+		$(this).children('.description').stop().fadeTo(500, 1);
+	},function(){
+		//when mouse out of the wrapper div
+		//use fadeTo to hide the div
+		$(this).children('.description').stop().fadeTo(500, 0);
+	});
+	
+});
